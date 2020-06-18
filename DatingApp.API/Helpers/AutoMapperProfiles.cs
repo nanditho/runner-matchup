47608b00a5def2b9b4659f2fed988515ee14ef5d
teddysmithdev <teddysmithdev@gmail.com>
2020-06-18 07:41:20 -0400

    delete and main photo endpoints

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -16,6 +16,9 @@ namespace DatingApp.API.Helpers
          .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
          .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
       CreateMap<Photo, PhotosForDetailedDto>();
+      CreateMap<UserForUpdateDto, User>();
+      CreateMap<Photo, PhotoForReturnDto>();
+      CreateMap<PhotoForCreationDto, Photo>();
     }
   }
 }
